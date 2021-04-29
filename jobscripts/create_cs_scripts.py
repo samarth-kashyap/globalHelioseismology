e2430fa68385d2da216e643abb29ffae066c3238
Samarth G Kashyap <g.samarth@tifr.res.in>
2021-04-29 19:26:43 +0530

    jobscript creation complete

--- jobscripts/create_cs_scripts.py
+++ jobscripts/create_cs_scripts.py
@@ -3,34 +3,49 @@ import argparse
 import os, sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--n", help='radial order',
-                    type=int)
-parser.add_argument("--ps", help='power spectrum',
-                    action="store_true")
+parser.add_argument("--n", help='radial order', type=int)
+parser.add_argument("--dell", help='delta ell', type=int)
+parser.add_argument("--t", help='radial order', type=int)
 args = parser.parse_args()
 
 pythonpath = "/home/g.samarth/anaconda3/bin/python"
-execpath = "/home/g.samarth/Woodard2013/WoodardPy/cs_data.py"
+execpath = "/home/g.samarth/globalHelioseismology/cs_data.py"
+
 n = args.n
-l1_arr = np.load(f"/scratch/g.samarth/csfit/l1_{n:02d}.npy")
-l2_arr = np.load(f"/scratch/g.samarth/csfit/l2_{n:02d}.npy")
+dell = args.dell
+t = args.t
 
-if args.ps:
-    l_arr = np.append(l1_arr, l2_arr)
-    l_arr = np.unique(l_arr)
-    l_arr = np.sort(l_arr)
-    l1_arr = l_arr
-    l2_arr = l_arr
+data = np.loadtxt("/home/g.samarth/globalHelioseismology/mode-params/hmi.6328.36")
+ln_arr = data[:, :2]
+ln_list = ln_arr.astype('int').tolist()
 
+mask_radial = ln_arr[:, 1] == args.n
+larr = ln_arr[mask_radial, 0]
+l1arr = []
+l2arr = []
+
+for ell1 in larr:
+    ell2 = ell1 + dell
+    mode_found = True
+    try:
+        ln_list.index([ell2, args.n])
+    except ValueError:
+        print(f" Mode not found ell = {ell2}; n = {args.n}")
+        mode_found = False
+
+    if mode_found:
+        l1arr.append(ell1)
+        l2arr.append(ell2)
+
+l1_arr = np.array(l1arr)
+l2_arr = np.array(l2arr)
 len_ell = len(l1_arr)
-if args.ps:
-    filename = f"/home/g.samarth/Woodard2013/job_scripts/ipjobs_ps_{n:02d}.sh"
-else:
-    filename = f"/home/g.samarth/Woodard2013/job_scripts/ipjobs_cs_{n:02d}.sh"
+
+filename = f"/home/g.samarth/globalHelioseismology/jobscripts/ipjobs_cs_{n:02d}.sh"
 
 with open(filename, "w") as f:
     for i in range(len_ell):
         l1, l2 = int(l1_arr[i]), int(l2_arr[i])
-        exec_cmd = (f"{pythonpath} {execpath} --l {l1} --lp {l2} --n {n} --np {n} " +
-                    f"--find_baselines\n")  #--plot
+        exec_cmd = (f"{pythonpath} {execpath} --l {l1} --lp {l2} --n {n} " +
+                    f"--np {n} --t {args.t} --find_baselines\n")  #--plot
         f.write(exec_cmd)
