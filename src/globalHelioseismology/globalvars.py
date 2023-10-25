dcc265059213049cc80c8c9951f45af53573fc81
Samarth Kashyap <samarth.g.kashyap@outlook.com>
2023-10-25 17:05:18 +0200

    BUGFIX: the .config file directory fixed

--- src/globalHelioseismology/globalvars.py
+++ src/globalHelioseismology/globalvars.py
@@ -1,7 +1,7 @@
 import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-package_dir = os.path.dirname(current_dir)
+package_dir = os.path.dirname(os.path.dirname(current_dir))
 with open(f"{package_dir}/.config", "r") as f:
     dirnames = f.read().splitlines()
 print(f"dirnames = {dirnames}")
